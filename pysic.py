ae13d811c2dd96083795d58ae5bcd6b4ee38d7cd
Avantgarde95 <95phm@kaist.ac.kr>
2016-06-06 02:09:37 +0100

    Modified typo

---
+++ pysic.py
@@ -0,0 +1,53 @@
+import sys, os
+from pysic.lib.shell import PsShell
+from pysic.lib.parse import PsParser
+from pysic.lib.interpret import PsInterpreter
+
+def print_usage():
+    print 'Usage : python pysic.py source.psc\n'
+
+if __name__ == '__main__':
+    # check the number of arguments
+    if len(sys.argv) == 1:
+        print_usage()
+        sys.exit(1)
+
+    # check the extension
+    filename = sys.argv[1]
+
+    if not filename.endswith('.psc'):
+        print "Error : Source extension should be '.psc'!"
+        sys.exit(2)
+
+    # open the source file
+    try:
+        #curpath = os.path.dirname(__file__)
+        #relpath = os.path.relpath(filename, '.')
+        #fullpath = os.path.join(curpath, filename)
+        # print curpath, fullpath
+        p = open(filename, 'r')
+    except IOError:
+        print "Error : Can't open the source file!"
+        sys.exit(3)
+
+    source = p.read()
+    p.close()
+
+    ps = PsShell()
+    parser = PsParser()
+    interpreter = PsInterpreter(
+        func = {
+            'stdin' : ps.input,
+            'stdout' : ps.output,
+            'stderr' : ps.error
+        }
+    )
+
+    ps.start()
+
+    interpreter.digest(*parser.parse(source))
+    interpreter.run()
+
+    ps.end()
+
+    sys.exit(0)
